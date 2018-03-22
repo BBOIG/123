32de0d63063d1860aae1c7a2939300241cb0897d
m305-68\w401 <ewfef@534350.154>
2018-03-22 15:56:23 +0800

    2

--- lottery/Program.cs
+++ lottery/Program.cs
@@ -10,6 +10,14 @@ namespace lottery
     {
         static void Main(string[] args)
         {
+            // 產生亂數物件
+            Random rand = new Random();
+            
+            // 印出亂數
+            Console.WriteLine(rand.Next(50, 100));
+
+            // 暫停螢幕
+            Console.ReadLine();
         }
     }
 }
