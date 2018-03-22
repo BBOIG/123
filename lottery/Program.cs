98729b8916735b1c5d886466c406adfb93a8fd80
m305-68\w401 <ewfef@534350.154>
2018-03-22 16:16:07 +0800

    4

--- lottery/Program.cs
+++ lottery/Program.cs
@@ -20,10 +20,9 @@ namespace lottery
             int r4 = rand.Next(0, 43);
             int r5 = rand.Next(0, 43);
             int r6 = rand.Next(0, 43);
-            int r7 = rand.Next(0, 43);
             
             // 顯示
-            Console.WriteLine("號碼:{0:00} {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}",r1, r2, r3, r4, r5, r6, r7);  
+            Console.WriteLine("號碼:{0:00} {1:00} {2:00} {3:00} {4:00} {5:00}",r1, r2, r3, r4, r5, r6);  
 
             // 暫停螢幕
             Console.ReadLine();
