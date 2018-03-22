2eaed97c88b8988ba846df4c755ff52bea792090
m305-68\w401 <ewfef@534350.154>
2018-03-22 16:22:46 +0800

    加入迴圈

--- lottery/Program.cs
+++ lottery/Program.cs
@@ -13,19 +13,24 @@ namespace lottery
             // 產生亂數物件
             Random rand = new Random();
 
-            // 印出亂數
-            int r1 = rand.Next(0, 43);
-            int r2 = rand.Next(0, 43);
-            int r3 = rand.Next(0, 43);
-            int r4 = rand.Next(0, 43);
-            int r5 = rand.Next(0, 43);
-            int r6 = rand.Next(0, 43);
-            
-            // 顯示
-            Console.WriteLine("號碼:{0:00} {1:00} {2:00} {3:00} {4:00} {5:00}",r1, r2, r3, r4, r5, r6);  
+            while (true)
+            {
 
-            // 暫停螢幕
-            Console.ReadLine();
+                // 印出亂數
+                int r1 = rand.Next(0, 43);
+                int r2 = rand.Next(0, 43);
+                int r3 = rand.Next(0, 43);
+                int r4 = rand.Next(0, 43);
+                int r5 = rand.Next(0, 43);
+                int r6 = rand.Next(0, 43);
+
+                // 顯示
+                Console.WriteLine("號碼:{0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", r1, r2, r3, r4, r5, r6);
+
+                // 暫停螢幕
+                Console.ReadLine();
+            }
+
         }
     }
 }
